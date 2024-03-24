90fed8182512726589b98279d4dc519e691b0d9e
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-03-24 02:46:09 -0500

    Added biome lang keys

--- src/main/java/luke/bonusblocks/biomes/ModBiomes.java
+++ src/main/java/luke/bonusblocks/biomes/ModBiomes.java
@@ -7,31 +7,31 @@ import net.minecraft.core.world.biome.Biomes;
 public abstract class ModBiomes {
     public static final Biome OVERWORLD_OVERGROWN_MEADOW =
             Biomes.register(BonusBlocksMod.MOD_ID+":overworld.overgrown.meadow",
-                    new BiomeOvergrown("bonusblocks:overworld.overgrown.meadow"));
+                    new BiomeOvergrown("bonusblocks.overgrown.meadow"));
 
     public static final Biome OVERWORLD_OUTBACK_MESA =
             Biomes.register(BonusBlocksMod.MOD_ID+":overworld.outback.mesa",
-                    new BiomeMesa("bonusblocks:overworld.outback.mesa"));
+                    new BiomeMesa("bonusblocks.outback.mesa"));
 
     public static final Biome OVERWORLD_DRYLAND =
             Biomes.register(BonusBlocksMod.MOD_ID+":overworld.dryland",
-                    new BiomeDryland("bonusblocks:overworld.dryland"));
+                    new BiomeDryland("bonusblocks.dryland"));
 
     public static final Biome OVERWORLD_WETLAND =
             Biomes.register(BonusBlocksMod.MOD_ID+":overworld.wetland",
-                    new BiomeWetland("bonusblocks:overworld.wetland"));
+                    new BiomeWetland("bonusblocks.wetland"));
 
     public static final Biome OVERWORLD_MAPLE =
             Biomes.register(BonusBlocksMod.MOD_ID+":overworld.maple",
-                    new BiomeMaple("bonusblocks:overworld.maple"));
+                    new BiomeMaple("bonusblocks.maple"));
 
 
 
     public static final Biome NETHER_SOUL_DESERT =
             Biomes.register(BonusBlocksMod.MOD_ID+":nether.soul.desert",
-                    new BiomeSoulDesert("bonusblocks:nether.soul.desert"));
+                    new BiomeSoulDesert("bonusblocks.soul.desert"));
 
     public static final Biome NETHER_SOUL_WASTELAND =
             Biomes.register(BonusBlocksMod.MOD_ID+":nether.soul.wasteland",
-                    new BiomeSoulWasteland("bonusblocks:nether.soul.wasteland"));
+                    new BiomeSoulWasteland("bonusblocks.soul.wasteland"));
 }
