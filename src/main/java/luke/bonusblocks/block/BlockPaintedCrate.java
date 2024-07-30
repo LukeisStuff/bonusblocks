50ba7cac8b5ebb5e9fabe6aaba71c0cf7d747879
LukeisStuff <lukeisawesome03@gmail.com>
2024-07-30 12:36:49 -0400

    updating to 7.2

--- src/main/java/luke/bonusblocks/block/BlockPaintedCrate.java
+++ src/main/java/luke/bonusblocks/block/BlockPaintedCrate.java
@@ -5,10 +5,7 @@ import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.block.material.Material;
 import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
-
-import static luke.bonusblocks.BonusBlocksMod.MOD_ID;
 
 public class BlockPaintedCrate extends Block {
 
