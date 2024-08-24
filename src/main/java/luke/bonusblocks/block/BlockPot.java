0419b194be28d9c25b0a92458779a219b706e757
LukeisStuff <lukeisawesome03@gmail.com>
2024-08-24 11:48:53 -0400

    better pot

--- src/main/java/luke/bonusblocks/block/BlockPot.java
+++ src/main/java/luke/bonusblocks/block/BlockPot.java
@@ -2,14 +2,14 @@ package luke.bonusblocks.block;
 
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
-import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.world.World;
 
 public class BlockPot extends Block {
 
     public BlockPot(String key, int id, Material material) {
         super(key, id, material);
-        this.setBlockBounds(0.25F, 0.0F, 0.25F, 0.75F, 0.9375f, 0.75F);
+        float f = 0.0625f;
+        this.setBlockBounds(0.0625f + f, 0.0F, 0.0625f + f, 0.9375f - f, 1.0f, 0.9375f - f);
     }
 
     public boolean isSolidRender() {
@@ -20,24 +20,8 @@ public class BlockPot extends Block {
         return false;
     }
 
-    public void onBlockAdded(World world, int x, int y, int z) {
-        world.isBlockNormalCube(x, y - 1, z);
-    }
-
     public boolean canPlaceBlockAt(World world, int x, int y, int z) {
         return world.isBlockNormalCube(x, y - 1, z) || world.canPlaceOnSurfaceOfBlock(x, y - 1, z);
-    }
-
-    public void onNeighborBlockChange(World world, int x, int y, int z, int blockId) {
-        if (!this.canBlockStay(world, x, y, z)) {
-            this.dropBlockWithCause(world, EnumDropCause.WORLD, x, y, z, world.getBlockMetadata(x, y, z), null);
-            world.setBlockWithNotify(x, y, z, 0);
-        }
-
-    }
-
-    public boolean canBlockStay(World world, int x, int y, int z) {
-        return world.canPlaceOnSurfaceOfBlock(x, y - 1, z);
     }
 
 }
