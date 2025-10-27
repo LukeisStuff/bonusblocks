1c03ca9756f28458cbda12bd4ad9f4a82ca60916
LukeisStuff <lukeisawesome03@gmail.com>
2025-10-27 13:23:09 -0400

    fix mixin incompat with aether

--- src/main/java/luke/bonusblocks/mixin/EntityClothBounceMixin.java
+++ src/main/java/luke/bonusblocks/mixin/EntityClothBounceMixin.java
@@ -1,17 +1,17 @@
 package luke.bonusblocks.mixin;
 
 import net.minecraft.core.entity.Entity;
-import net.minecraft.core.util.helper.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Entity.class, remap = false)
 public class EntityClothBounceMixin {
+
     @Shadow
     public double y;
     @Unique
@@ -25,9 +25,8 @@ public class EntityClothBounceMixin {
         prevYBB = y;
     }
 
-    @Redirect(method = "move(DDD)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/util/helper/MathHelper;floor(D)I", ordinal = 5))
-    public int extendBlockRangeBB(double xd, double yd, double zd) {
-        return MathHelper.floor(((Entity) (Object) this).bb.minY + 0.001 + deltaYBB);
+    @ModifyArg(method = "move(DDD)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/util/helper/MathHelper;floor(D)I", ordinal = 5), index = 0)
+    public double extendBlockRangeBB(double originalY) {
+        return originalY + deltaYBB;
     }
-
 }
